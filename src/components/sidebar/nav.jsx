d8194fe5e1f58e538bc991df035c5e3f1f8c4e68
Erjan18 <johnydepp2002@mail.ru>
2020-12-17 13:07:39 +0600

    add sidebar

--- src/components/sidebar/nav.jsx
+++ src/components/sidebar/nav.jsx
@@ -1,12 +1,19 @@
-import React from 'react';
+import React from 'react'; 
 import { navNames, navIcons } from './const';
+import {Link} from 'react-router-dom' 
 
 const Nav = () => {
+  const [itemActive, setItemActive] = React.useState(0);
+  const handleClick = (index) => {
+    setItemActive(index)
+  };
+
   return (
     <nav className='sidebar__nav'>
       <ul>
         {navNames.map((item, index) => {
           return (
+            // onclick={() => handleClick(index)}
             <li className='sidebar__item'>
               <a className='sidebar__link' href='#'>
                 <img src={navIcons[index]} alt='nav-icon' />
