40d17506144c5dd2c5159994ceff04c266e90d54
Ashymaliev <nursultan-99kg@mail.ru>
2020-12-23 13:39:00 +0600

    react #8

--- src/pages/manage/index.js
+++ src/pages/manage/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import axios from 'axios';
 import TableData from './tableData';
-import SearchBar from './searchBar'
-import {PrimaryBtn} from '../../components/btns';
+import SearchBar from './searchBar';
+import { PrimaryBtn } from '../../components/btns';
+import Modal from './modal';
 import './style.scss';
 
 export default function Manage() {
   const [usersData, setUsersData] = React.useState([]);
+  const [activeModal, setActiveModal] = React.useState(false);
+
+  const handleClickModal = () => {
+    setActiveModal(true);
+  };
+
   React.useEffect(async () => {
     await axios
       .get(`http://localhost:3000/database.json`)
@@ -15,14 +22,22 @@ export default function Manage() {
   }, []);
   return (
     <div className='manage'>
-    <div className='manage__top'>
-    <SearchBar style={{borderRadius: '4px'}}/>
-      <PrimaryBtn text={'Add user'} />
-    </div>
-      <div style={{ height: 450, width: '100%', background: 'white', borderRadius: '4px'}}>
-        <TableData ayday={usersData}/>
+      {activeModal && (
+        <Modal
+          usersData={usersData}
+          activeModal={activeModal}
+          setActiveModal={setActiveModal}
+          setUsersData={setUsersData}
+        />
+      )}
+
+      <div className='manage__top'>
+        <SearchBar style={{ borderRadius: '4px' }} />
+        <PrimaryBtn handleClickModal={handleClickModal} text={'Add user'} />
+      </div>
+      <div style={{ height: 450, width: '100%', background: 'white', borderRadius: '4px' }}>
+        <TableData users={usersData} />
       </div>
     </div>
-    
   );
 }
