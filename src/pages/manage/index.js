45599248c84cb13a3a606b1cf9fd25090a79c08d
Ashymaliev <nursultan-99kg@mail.ru>
2021-01-07 13:28:25 +0600

    add some sections

--- src/pages/manage/index.js
+++ src/pages/manage/index.js
@@ -1,3 +1,4 @@
+  
 import React from 'react';
 import axios from 'axios';
 import TableData from './tableData';
@@ -23,29 +24,31 @@ export default function Manage() {
   }, []);
   return (
     <div className='manage'>
-      {activeModal && (
-        <Modal
-          usersData={usersData}
-          activeModal={activeModal}
-          setActiveModal={setActiveModal}
-          setUsersData={setUsersData}
-        />
-      )}
+      <div className='section__container'>
+        {activeModal && (
+          <Modal
+            usersData={usersData}
+            activeModal={activeModal}
+            setActiveModal={setActiveModal}
+            setUsersData={setUsersData}
+          />
+        )}
 
-      <div className='manage__top'>
-        <SearchBar setSearchValue={setSearchValue} style={{ borderRadius: '4px' }} />
-        <PrimaryBtn handleClickModal={handleClickModal} text={'Add user'} />
-      </div>
-      <div style={{ height: 450, width: '100%', background: 'white', borderRadius: '4px' }}>
-        <TableData
-          users={usersData.filter((item) => {
-            if (searchValue === '') {
-              return item;
-            } else if (item.fullName.toLowerCase().includes(searchValue.toLowerCase())) {
-              return item;
-            }
-          })}
-        />
+        <div className='manage__top'>
+          <SearchBar setSearchValue={setSearchValue} style={{ borderRadius: '4px' }} />
+          <PrimaryBtn handleClickModal={handleClickModal} text={'Add user'} />
+        </div>
+        <div style={{ height: 450, width: '100%', background: 'white', borderRadius: '4px' }}>
+          <TableData
+            users={usersData.filter((item) => {
+              if (searchValue === '') {
+                return item;
+              } else if (item.fullName.toLowerCase().includes(searchValue.toLowerCase())) {
+                return item;
+              }
+            })}
+          />
+        </div>
       </div>
     </div>
   );
